ff9471df59db2510d6a46dac804056a7a0787b21
James Friel <JFriel001@dundee.ac.uk>
2024-10-10 10:47:26 +0100

    add title

--- src/components/QuestionScreen.js
+++ src/components/QuestionScreen.js
@@ -3,7 +3,9 @@ import {Typography} from "@mui/material";
 
 function QuestionScreen(props){
     var question = props.question;
-    return <div style={{display:'flex', background:'#1976d2', borderRadius:8,height:100,justifyContent:'center', alignItems:'center', width:'80%'}}>
+    var showTitle = props.hideTitle?false:true;
+    return <div style={{background:'#1976d2', borderRadius:8,height:100,justifyContent:'center', alignItems:'center', width:'80%', display:'flex',flexDirection:'column'}}>
+        {showTitle && <Typography style={{color:'white', margin:8}} variant={"h5"}>Question:</Typography>}
         <Typography style={{color:'white', margin:8}}>{question}</Typography>
     </div>
 }
--- src/components/RequirementsScreen.js
+++ src/components/RequirementsScreen.js
@@ -3,7 +3,9 @@ import {Typography} from "@mui/material";
 
 function RequirementsScreen(props){
     var requirements = props.requirements;
-    return <div style={{display:'flex', background:'#ff7961', borderRadius:8,height:100,justifyContent:'center', alignItems:'center', width:'80%'}}>
+    return <div style={{ background:'#ff7961', borderRadius:8,height:100,justifyContent:'center', alignItems:'center', width:'80%', display:'flex',flexDirection:'column'}}>
+        <Typography style={{color:'white', margin:8}} variant={"h5"}>Required Authorisations:</Typography>
+
         <Typography style={{color:'white', margin:8}}>{requirements}</Typography>
     </div>
 }
